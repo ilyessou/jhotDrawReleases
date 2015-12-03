4e2471feec1dc9b57ab30eb29f41f99fb94731da
elyes <elyeslahmer@gmail.com>
2015-12-02 19:47:08 -0500

    jhotDraw_7.3

--- jhotdraw7/src/main/java/org/jhotdraw/draw/FigureLayerComparator.java
+++ jhotdraw7/src/main/java/org/jhotdraw/draw/FigureLayerComparator.java
@@ -1,5 +1,5 @@
 /*
- * @(#)FigureLayerComparator.java  1.0  1. April 2004
+ * @(#)FigureLayerComparator.java
  *
  * Copyright (c) 1996-2006 by the original authors of JHotDraw
  * and all its contributors.
@@ -16,10 +16,10 @@ package org.jhotdraw.draw;
 
 import java.util.*;
 /**
- * FigureLayerComparator.
+ * A {@code Comparator} used to sort figures by their layer property.
  *
  * @author  Werner Randelshofer
- * @version 1.0 1. April 2004  Created.
+ * @version $Id$
  */
 public class FigureLayerComparator implements Comparator<Figure> {
     public final static FigureLayerComparator INSTANCE = new FigureLayerComparator();
